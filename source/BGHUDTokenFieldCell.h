539440081914feeeaac0416acfba8dbc7b56f6c3
Tim Davis <binary.god@gmail.com>
2008-06-11 04:15:32 +0000

    Added BGHUDTokenFieldCell *DOES NOT WORK YET*

--- source/BGHUDTokenFieldCell.h
+++ source/BGHUDTokenFieldCell.h
@@ -1,8 +1,8 @@
 //
-//  BGHUDAppKit.h
+//  BGHUDTokenFieldCell.h
 //  BGHUDAppKit
 //
-//  Created by BinaryGod on 5/25/08.
+//  Created by BinaryGod on 6/10/08.
 //
 //  Copyright (c) 2008, Tim Davis (BinaryMethod.com, binary.god@gmail.com)
 //  All rights reserved.
@@ -32,14 +32,10 @@
 //	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 //	POSSIBILITY OF SUCH DAMAGE.
 
-#import "BGHUDScroller.h"
-#import "BGHUDButtonCell.h"
-#import "BGHUDGradientButtonCell.h"
-#import "BGHUDSliderCell.h"
-#import "BGHUDScrollView.h"
-#import "BGHUDPopUpButtonCell.h"
-#import "BGHUDGradientPopUpButtonCell.h"
-#import "BGHUDTextFieldCell.h"
-#import "BGHUDGradientTextFieldCell.h"
-#import "BGHUDProgressIndicator.h"
-#import "BGHUDGradientProgressIndicator.h"
+#import <Cocoa/Cocoa.h>
+
+@interface BGHUDTokenFieldCell : NSTokenFieldCell {
+
+}
+
+@end
--- source/BGHUDTokenFieldCell.m
+++ source/BGHUDTokenFieldCell.m
@@ -1,8 +1,8 @@
 //
-//  BGHUDAppKit.h
+//  BGHUDTokenFieldCell.m
 //  BGHUDAppKit
 //
-//  Created by BinaryGod on 5/25/08.
+//  Created by BinaryGod on 6/10/08.
 //
 //  Copyright (c) 2008, Tim Davis (BinaryMethod.com, binary.god@gmail.com)
 //  All rights reserved.
@@ -32,14 +32,11 @@
 //	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 //	POSSIBILITY OF SUCH DAMAGE.
 
-#import "BGHUDScroller.h"
-#import "BGHUDButtonCell.h"
-#import "BGHUDGradientButtonCell.h"
-#import "BGHUDSliderCell.h"
-#import "BGHUDScrollView.h"
-#import "BGHUDPopUpButtonCell.h"
-#import "BGHUDGradientPopUpButtonCell.h"
-#import "BGHUDTextFieldCell.h"
-#import "BGHUDGradientTextFieldCell.h"
-#import "BGHUDProgressIndicator.h"
-#import "BGHUDGradientProgressIndicator.h"
+#import "BGHUDTokenFieldCell.h"
+
+
+@implementation BGHUDTokenFieldCell
+
+
+
+@end
