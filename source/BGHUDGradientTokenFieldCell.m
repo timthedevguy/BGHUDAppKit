f5e87c3eb19795307f399357f83c7a815b48cfde
Tim Davis <binary.god@gmail.com>
2008-06-12 19:29:33 +0000

    Added Gradient varient of BGHUDTokenFieldCell

--- source/BGHUDGradientTokenFieldCell.m
+++ source/BGHUDGradientTokenFieldCell.m
@@ -1,8 +1,8 @@
 //
-//  BGHUDAppKit.h
+//  BGHUDGradientTokenFieldCell.m
 //  BGHUDAppKit
 //
-//  Created by BinaryGod on 5/25/08.
+//  Created by BinaryGod on 6/12/08.
 //
 //  Copyright (c) 2008, Tim Davis (BinaryMethod.com, binary.god@gmail.com)
 //  All rights reserved.
@@ -32,16 +32,14 @@
 //	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 //	POSSIBILITY OF SUCH DAMAGE.
 
-#import "BGHUDScroller.h"
-#import "BGHUDButtonCell.h"
-#import "BGHUDGradientButtonCell.h"
-#import "BGHUDSliderCell.h"
-#import "BGHUDScrollView.h"
-#import "BGHUDPopUpButtonCell.h"
-#import "BGHUDGradientPopUpButtonCell.h"
-#import "BGHUDTextFieldCell.h"
-#import "BGHUDGradientTextFieldCell.h"
-#import "BGHUDProgressIndicator.h"
-#import "BGHUDGradientProgressIndicator.h"
-#import "BGHUDTokenFieldCell.h"
 #import "BGHUDGradientTokenFieldCell.h"
+
+
+@implementation BGHUDGradientTokenFieldCell
+
+-(NSColor *)strokeColor {
+	
+	return [NSColor colorWithDeviceRed: 0.749 green: 0.761 blue: 0.788 alpha: 0.7];
+}
+
+@end
