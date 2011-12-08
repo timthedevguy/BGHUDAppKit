968de7fa487f9343383a831c61a729317059b58a
Jacob Gorban <jacob@apparentsoft.com>
2011-12-08 16:33:10 +0200

    MY changes.

--- Framework/BGTheme.m
+++ Framework/BGTheme.m
@@ -121,12 +121,12 @@
 
 -(NSColor *)selectionHighlightActiveColor {
 	
-	return [NSColor darkGrayColor];
+	return [NSColor grayColor];
 }
 
 -(NSColor *)selectionHighlightInActiveColor {
 	
-	return [NSColor darkGrayColor];
+	return [NSColor grayColor];
 }
 
 -(NSColor *)selectionTextActiveColor {
