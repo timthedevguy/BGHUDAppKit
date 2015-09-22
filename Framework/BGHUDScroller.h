a949b76054e238428fc1e619db3a7e5b90cd8153
philippm <philipp@incrediblebee.com>
2015-09-22 16:37:03 +0200

    Remove explicit property getter declaration
    The compiler implicitly uses the property name for the getter method.

--- Framework/BGHUDScroller.h
+++ Framework/BGHUDScroller.h
@@ -46,7 +46,7 @@
 	NSString *themeKey;
 }
 
-@property (copy,getter=themeKey) NSString *themeKey;
+@property (copy) NSString *themeKey;
 
 - (void)drawDecrementArrow:(BOOL)highlighted;
 - (void)drawIncrementArrow:(BOOL)highlighted;
